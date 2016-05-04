c7f124ef7dc206b05ce38a8fefbe7f702c4f36a0
PROTECO <proteco@comunidadproteco.proteco.mx>
2016-05-04 12:52:42 -0500

    Toast on marker was added

--- app/src/main/java/com/marco/mapas/MapsActivity.java
+++ app/src/main/java/com/marco/mapas/MapsActivity.java
@@ -5,6 +5,7 @@ import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
@@ -13,6 +14,7 @@ import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.BitmapDescriptor;
 import com.google.android.gms.maps.model.BitmapDescriptorFactory;
 import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
 
 public class MapsActivity extends FragmentActivity implements OnMapReadyCallback, View.OnClickListener {
@@ -91,10 +93,20 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
             @Override
             public void onMapLongClick(LatLng latLng) {
                 mMap.addMarker(new MarkerOptions()
-                        .icon(BitmapDescriptorFactory.fromResource(R.mipmap.marcador))
-                        .anchor(0.0f,1.0f)
-                        .position(latLng)
+                                .icon(BitmapDescriptorFactory.fromResource(R.mipmap.marcador))
+                                .anchor(0.0f, 1.0f)
+                                .position(latLng)
                 );
+            }
+        });
+
+        mMap.setOnMarkerClickListener(new GoogleMap.OnMarkerClickListener() {
+            @Override
+            public boolean onMarkerClick(Marker marker) {
+
+                Toast.makeText(getApplicationContext(),"Haz pulsado una marca",Toast.LENGTH_LONG).show();
+
+                return false;
             }
         });
     }
