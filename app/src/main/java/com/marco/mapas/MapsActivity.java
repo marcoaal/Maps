303ee0bd13859818b51563ed9c7dd74b748de4d8
PROTECO <proteco@comunidadproteco.proteco.mx>
2016-05-03 20:41:41 -0500

    Button functionality was added

--- app/src/main/java/com/marco/mapas/MapsActivity.java
+++ app/src/main/java/com/marco/mapas/MapsActivity.java
@@ -2,6 +2,7 @@ package com.marco.mapas;
 
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
@@ -11,7 +12,7 @@ import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 
-public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
+public class MapsActivity extends FragmentActivity implements OnMapReadyCallback, View.OnClickListener {
 
     private GoogleMap mMap;
 
@@ -28,12 +29,32 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         bterreno = (Button)findViewById(R.id.bterreno);
         bhibrido = (Button)findViewById(R.id.bhibrido);
 
+        bmapa.setOnClickListener(this);
+        bterreno.setOnClickListener(this);
+        bhibrido.setOnClickListener(this);
+
         // Obtain the SupportMapFragment and get notified when the map is ready to be used.
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
     }
 
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()){
+            case R.id.bmapa:
+                mMap.setMapType(GoogleMap.MAP_TYPE_SATELLITE);
+            break;
+
+            case R.id.bterreno:
+                mMap.setMapType(GoogleMap.MAP_TYPE_HYBRID);
+            break;
+
+            case R.id.bhibrido:
+                mMap.setMapType(GoogleMap.MAP_TYPE_TERRAIN);
+            break;
+        }
+    }
 
     /**
      * Manipulates the map once available.
